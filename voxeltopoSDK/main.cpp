a19899f1b0f218f8da55bea7fc196898b1938299
danielyan86129 <danielyan86129@gmail.com>
2018-02-04 23:19:08 -0600

    Added code to make mean-value coordinates intensity volume - added gflags as external dependency - added support to add gaussian noise to volume - fixed a small issue in MVC paper formula ( w_i denominator should be multiplied by 2) - fixed mesh faces orientation issue (before, normals were still inward despite some attempt was made)

--- voxeltopoSDK/main.cpp
+++ voxeltopoSDK/main.cpp
@@ -1,77 +1,133 @@
 #include "synth_data_generator.h"
+#include <reader.h> // tao volume reader
+
 #include <iostream>
 #include <string>
+#include <map>
 #include <experimental/filesystem>
+#include <gflags/gflags.h>
 
 using std::cout;
 using std::endl;
 using std::string;
 namespace fs = std::experimental::filesystem;
 
-void veryifyArgs( int _argc, char ** _argv );
-void printUsage( int _argc, char ** _argv );
+/*all cmd line arguments*/
+enum ValidMode {
+	INTENSITY, SDF, NOISE
+};
+std::map<string, ValidMode> validmodes_map = {
+	{"i", ValidMode::INTENSITY},
+	{"s", ValidMode::SDF},
+	{ "n", ValidMode::NOISE }
+};
+// define cmd flag 'md' for modes & its handler
+DEFINE_string( md, "not-specified", "specify a mode for the utility to perform certain task. REQUIRED." );
+static bool processMode( const char* _flagname, const std::string& _value )
+{
+	if ( validmodes_map.find( _value ) == validmodes_map.end() )
+	{
+		cout << "Invalid value for -md: " << _value << endl;
+		return false;
+	}
+	return true;
+}
+DEFINE_validator( md, processMode );
+// options for mode "i" and "s"
+DEFINE_string( I, "not-specified", "input mesh for which certain function will be generated. REQUIRED." );
+DEFINE_double( r, 0, "resolution of output volume. REQUIRED." );
+// options for mode "n"
+DEFINE_string( V, "not-specified", "input volume for which certain operation will be performed. REQUIRED." );
+DEFINE_double( ns, 0.001, "noise scale (w.r.t. value range in given volume). OPTIONAL." );
+
+void printUsage();
 int main( int _argc, char ** _argv )
 {
-	veryifyArgs( _argc, _argv );
-	/* figure out input file and construct volume's output filename */
-	std::string mode = _argv[ 1 ];
-	int res = std::stoi( _argv[ 2 ] );
-	fs::path mesh_filename = fs::path( _argv[ 3 ] );
-	auto vol_filename = mesh_filename; 
-	vol_filename.replace_extension( ".mrc" );
-	/* read in mesh */
-	TriMesh *mesh = TriMesh::read( mesh_filename.string() );
-	if ( !mesh )
-	{
-		cout << "Error: Cannot read mesh from file " << mesh_filename << endl;
-		exit( -1 );
-	}
-	mesh->need_bbox();
-	mesh->need_normals();
-	
-	/* now make volume */
-	Volume vol( res, res, res );
-	cout << "Volume space alloc.ed." << endl;
-	if ( mode.compare( "-i" ) == 0 )
-	{
-		cout << "computing space transform: volume -> mesh..." << endl;
-		auto vol_box = synthdata::Generator::mkBBoxForVol( &vol );
-		auto vol2mesh = synthdata::Generator::computeSpaceTransform( vol_box, mesh->bbox );
-		cout << "making intensity volume" << endl;
-		synthdata::Generator::mkIntensityVol( mesh, vol2mesh, &vol, synthdata::SimpleInten() );
-	}
-	else if ( mode.compare( "-s" ) == 0 )
-	{
-		cout << "making SDF volume" << endl;
-		synthdata::Generator::mkSDFVol( mesh, &vol );
-	}
+	google::SetUsageMessage( "\
+		progexe -md=<a valid mode> <args...> \n\
+		args depend on each mode. \n\
+		" );
+	google::ParseCommandLineFlags( &_argc, &_argv, true );
 
-	/* clean-up */
-	delete mesh;
+	if ( FLAGS_md == "i" || FLAGS_md == "s" )
+	{
+		/* figure out input file and construct volume's output filename */
+		int res = FLAGS_r;
+		fs::path mesh_filename = FLAGS_I;
+		auto vol_filename = mesh_filename;
+		vol_filename.replace_extension( ".mrc" );
+		/* read in mesh */
+		TriMesh *mesh = TriMesh::read( mesh_filename.string() );
+		if ( !mesh )
+		{
+			cout << "Error: Cannot read mesh from file " << mesh_filename << endl;
+			exit( -1 );
+		}
+		mesh->need_bbox();
+		mesh->need_normals();
+		synthdata::Generator::enforceNormalsOutward( mesh );
 
-	/* output volume */
-	cout << "writing volume to file: " << vol_filename << endl;
-	int len = vol_filename.string().size() + 100;
-	char* vol_filename_cpy = new char[ len ];
-	memset( vol_filename_cpy, '\0', len );
-	memcpy( vol_filename_cpy, vol_filename.string().c_str(), vol_filename.string().size() );
-	vol.toMRCFile( vol_filename_cpy );
-	delete vol_filename_cpy;
+		/* now make volume */
+		Volume vol( res, res, res );
+		cout << "Volume space alloc.ed." << endl;
+		if ( FLAGS_md == "i" )
+		{
+			cout << "computing space transform: volume -> mesh..." << endl;
+			auto vol_box = synthdata::Generator::mkBBoxForVol( &vol );
+			auto vol2mesh = synthdata::Generator::computeSpaceTransform( vol_box, mesh->bbox );
+			cout << "making intensity volume" << endl;
+			synthdata::Generator::mkIntensityVol( mesh, vol2mesh, &vol, synthdata::MVCInten() );
+		}
+		else if ( FLAGS_md == "s" )
+		{
+			cout << "making SDF volume" << endl;
+			synthdata::Generator::mkSDFVol( mesh, &vol );
+		}
+
+		/* clean-up */
+		delete mesh;
+
+		/* output volume */
+		cout << "writing volume to file: " << vol_filename << endl;
+		int len = vol_filename.string().size() + 100;
+		char* vol_filename_cpy = new char[ len ];
+		memset( vol_filename_cpy, '\0', len );
+		memcpy( vol_filename_cpy, vol_filename.string().c_str(), vol_filename.string().size() );
+		vol.toMRCFile( vol_filename_cpy );
+		delete vol_filename_cpy;
+	}
+	else if ( FLAGS_md == "n" )
+	{
+		fs::path vol_in_fn = FLAGS_V;
+		fs::path vol_out_fn = vol_in_fn;
+		auto ext = vol_in_fn.extension().string();
+		vol_out_fn.replace_filename( vol_in_fn.filename().stem().string() + "_n" + ext );
+
+		int len = vol_in_fn.string().size() + 100;
+		char * tmp_fn = new char[ len ];
+		memset( tmp_fn, '\0', len );
+		memcpy( tmp_fn, vol_in_fn.string().c_str(), vol_in_fn.string().size() );
+		MRCReader mrc_reader( tmp_fn );
+
+		Volume* vol = mrc_reader.getVolume();
+		double g_v = ( vol->getMax() - vol->getMin() ) * FLAGS_ns;
+		printf( "adding noise (scale=%f) to volume... \n", g_v );
+		synthdata::Generator::addNoise( vol, g_v );
+		printf( "Done adding noise. \n" );
+		len = vol_out_fn.string().size() + 100;
+		memset( tmp_fn, '\0', len );
+		memcpy( tmp_fn, vol_out_fn.string().c_str(), vol_out_fn.string().size() );
+		printf( "Outputting volume to %s ...\n", tmp_fn );
+		vol->toMRCFile( tmp_fn );
+		printf( "Done outputting volume. \n" );
+		delete tmp_fn;
+		delete vol;
+	}
 
 	return 0;
 }
 
-void veryifyArgs( int _argc, char ** _argv )
-{
-	if ( _argc < 4 )
-	{
-		printUsage( _argc, _argv );
-		exit( -1 );
-	}
-}
-
 void printUsage( int _argc, char ** _argv )
 {
-	cout << "usage: " << _argv[ 0 ] << "<mode> <vol-res> <input-mesh-name>. " << endl 
-		<< "mode can be - i( intensity ) or -s( sdf )." << endl;
+	cout << google::ProgramUsage() << endl;
 }
--- voxeltopoSDK/synth_data_generator.h
+++ voxeltopoSDK/synth_data_generator.h
@@ -3,12 +3,18 @@
 #include <TriMesh_algo.h>
 #include <Volume.h> //Tao's volume
 #include <spaceinfo.h>
+#include <geomalgo.h>
+
 #include <functional>
+#include <random>
 #include <limits>
+#include <cmath>
 
 typedef trimesh::TriMesh TriMesh;
 typedef trimesh::TriMesh::Face TriFace;
 using trimesh::point;
+typedef trimesh::Vec<3, double> dvec3;
+typedef trimesh::Vec<2, double> dvec2;
 using trimesh::vec3;
 using trimesh::vec2;
 using trimesh::ivec3;
@@ -22,8 +28,11 @@ namespace synthdata
 	/*
 	** common defs
 	*/
+#define _USE_MATH_DEFINES
 	// epsilon
 	double eps = 0.000000001;
+	// compute determinant of 3x3 (upper-left-sub-)matrix (input must be at least 3x3)
+	double det3x3( const xform& _m );
 
 	// the function: a triangle -> (intensity,weight)
 	// used to derive an intensity volume given a mesh
@@ -40,16 +49,95 @@ namespace synthdata
 			trimesh::normalize( f_nml );
 			float scalar = f_nml.dot( v2c );
 			float w = 1.0 / std::max( (double)trimesh::dist2( c, _v ), eps );
-			return vec2( scalar, w );
+			return vec2( scalar*w, w );
 		}
 	};
 	struct MVCInten
 	{
-		vec2 operator() ( TriMesh* _msh, int _fi, point _v )
+		inline int i_m_1( int _i ) { return ( _i - 1 + 3 ) % 3; }
+		inline int i_p_1( int _i ) { return ( _i + 1 ) % 3; }
+		vec2 operator() ( TriMesh* _msh, int _fi, point _x )
 		{
-			return vec2( 1.0f );
+			double cur_eps = _msh->bbox.size().max() * eps;
+			auto f = _msh->faces[ _fi ];
+			double w = 0.0, scalar = 0.0;
+			vec3 u[ 3 ];
+			double d[ 3 ];
+			for ( auto i = 0; i < 3; ++i )
+			{
+				auto pi = _msh->vertices[ f[i] ];
+				d[i] = trimesh::dist( pi, _x );
+				if ( d[i] < cur_eps )
+				{
+					w = numeric_limits<float>::max();
+					scalar = 1.0f;
+					goto MVCIntenRET;
+				}
+				u[ i ] = ( pi - _x ) / (float)d[ i ];
+			}
+			double theta[ 3 ];
+			for ( auto i = 0; i < 3; ++i )
+			{
+				auto li = trimesh::len( u[ i_p_1( i ) ] - u[ i_m_1( i ) ] );
+				theta[i] = 2 * asin( li / 2.0 );
+			}
+			auto h = ( theta[ 0 ] + theta[ 1 ] + theta[ 2 ] ) / 2.0f;
+			if ( util::is_equal( M_PI, (double)h, (double)eps ) )
+			{
+				// x lies on this face, use 2d barycentric
+				for ( auto i = 0; i < 3; ++i )
+				{
+					auto wi = std::sin( theta[ i ] ) * d[ i_m_1( i ) ] * d[ i_p_1( i ) ];
+					scalar += wi;
+				}
+				w = numeric_limits<float>::max();
+				goto MVCIntenRET;
+			}
+			{
+				double c[ 3 ], s[ 3 ];
+				xform orient_mat(
+					u[ 0 ][ 0 ], u[ 0 ][ 1 ], u[ 0 ][ 2 ], 0.f,
+					u[ 1 ][ 0 ], u[ 1 ][ 1 ], u[ 1 ][ 2 ], 0.f,
+					u[ 2 ][ 0 ], u[ 2 ][ 1 ], u[ 2 ][ 2 ], 0.f,
+					0.f, 0.f, 0.f, 1.f );
+				double sign_orient = det3x3( orient_mat ) > 0.0 ? 1 : -1;
+				for ( auto i = 0; i < 3; ++i )
+				{
+					c[ i ] = ( 2 * std::sin( h ) * std::sin( h - theta[ i ] ) ) /
+						( std::sin( theta[ i_p_1( i ) ] ) * std::sin( theta[ i_m_1( i ) ] ) )
+						- 1.0;
+					s[ i ] = sign_orient * std::sqrt( std::max( eps, 1.0 - c[ i ] * c[ i ] ) );
+					if ( util::is_equal( s[ i ], 0.0, cur_eps ) )
+					{
+						// ignore this face since x on same plane but is outside
+						scalar = 0.0;
+						w = 0.0;
+						goto MVCIntenRET;
+					}
+				}
+				for ( auto i = 0; i < 3; ++i )
+				{
+					double wi = ( theta[ i ] - c[ i_p_1( i ) ] * theta[ i_m_1( i ) ] - c[ i_m_1( i ) ] * theta[ i_p_1( i ) ] ) /
+						2.0 * ( d[ i ] * std::sin( theta[ i_p_1( i ) ] )*s[ i_m_1( i ) ] );
+					scalar += wi;
+					w += wi;
+				}
+			}
+		MVCIntenRET:
+			return vec2( scalar, w );
 		}
 	};
+	/*
+	** implementation of common defs
+	*/
+	double det3x3( const xform& _m )
+	{
+		return 
+			_m( 0, 0 ) * ( _m( 1, 1 )*_m( 2, 2 ) - _m( 2, 1 )*_m( 1, 2 ) ) -
+			_m( 1, 0 ) * ( _m( 0, 1 )*_m( 2, 2 ) - _m( 2, 1 )*_m( 0, 2 ) ) +
+			_m( 2, 0 ) * ( _m( 0, 1 )*_m( 1, 2 ) - _m( 1, 1 )*_m( 0, 2 ) );
+	}
+
 	class Generator
 	{
 	private:
@@ -72,6 +160,10 @@ namespace synthdata
 		static void mkIntensityVol( TriMesh* _msh, const trimesh::xform& _vox2mesh, Volume* _vol, const intenFunc& _ifun );
 		// make a signed-distance-function volume
 		static void mkSDFVol( TriMesh* _msh, Volume* _vol );
+		/*
+		** operations on volume
+		*/
+		static void addNoise( Volume* _vol, double _g_v );
 	};
 
 	box3 Generator::mkBBoxForVol( Volume* _vol )
@@ -102,9 +194,21 @@ namespace synthdata
 				near_vi = vi;
 			}
 		}
-		// flip normals & face-orientation if closest point's normal is not outward
-		vec3 nrml = _msh->normals[ near_vi ];
-		if ( ( outside_p - _msh->vertices[ near_vi ] ).dot( nrml ) < 0.0f )
+		// flip normals & face-orientation if closest point's neighbor faces' normals are not outward
+		_msh->need_adjacentfaces();
+		const auto& adj_faces = _msh->adjacentfaces[ near_vi ];
+		auto near_v = _msh->vertices[ near_vi ];
+		bool good_orient = false;
+		for ( auto fi : adj_faces )
+		{
+			vec3 nrml = _msh->trinorm( fi );
+			if ( ( outside_p - near_v ).dot( nrml ) > 0.0f )
+			{
+				good_orient = true;
+				break;
+			}
+		}
+		if ( !good_orient )
 		{
 			trimesh::faceflip( _msh );
 			for ( auto& n : _msh->normals )
@@ -129,12 +233,22 @@ namespace synthdata
 						// accumulate intensity from each face to this voxel sample
 						for ( auto fi = 0; fi < faces.size(); ++fi )
 						{
+							/*if ( ii == 0 && jj == 0 && kk == 2 && fi == 472 )
+								int stop = 1;*/
 							auto f = faces[ fi ];
 							auto i_res = _ifun( _msh, fi, v );
-							intensity += i_res[0] * i_res[1];
+							if ( i_res[ 1 ] == numeric_limits<float>::max() )
+							{
+								intensity = i_res[ 0 ];
+								w = 1.0;
+								break;
+							}
+							intensity += i_res[0];
 							w += i_res[ 1 ];
 						}
-						intensity /= std::max( w, eps );
+						//intensity /= std::max( w, eps );
+						if ( intensity != intensity )
+							printf( "warning: intensity(%d,%d,%d)=%f\n", ii, jj, kk, intensity );
 						_vol->setDataAt( ii, jj, kk, intensity );
 					}
 				}
@@ -143,6 +257,36 @@ namespace synthdata
 	void Generator::mkSDFVol( TriMesh* _msh, Volume* _vol )
 	{
 
+	}
+	void Generator::addNoise( Volume* _vol, double _g_v )
+	{
+		// use constant seed for reproducible random sequence
+		unsigned seed = 1000; //std::chrono::system_clock::now().time_since_epoch().count();
+		std::default_random_engine generator( seed );
+		std::normal_distribution<double> guassian( 0.0, _g_v );
+		std::uniform_real_distribution<double> uniform( 0.0, 1.0 );
+		
+		double noise_min = numeric_limits<double>::max();
+		double noise_max = numeric_limits<double>::min();
+		for ( auto i = 0; i < _vol->getSizeX(); ++i )
+		{
+			for ( auto j = 0; j < _vol->getSizeY(); ++j )
+			{
+				for ( auto k = 0; k < _vol->getSizeZ(); ++k )
+				{
+					double old_v = _vol->getDataAt( i, j, k );
+					double noise = guassian( generator );
+					// Russian roulette to randomly add/discard noise
+					bool discard = uniform( generator ) > 0.5;
+					if ( discard )
+						continue;
+					noise_min = std::min( noise_min, noise );
+					noise_max = std::max( noise_max, noise );
+					_vol->setDataAt( i, j, k, old_v + noise );
+				}
+			}
+		}
+		printf( "added noise from range [%f, %f] \n", noise_min, noise_max );
 	}
 	xform Generator::computeSpaceTransform( const box3& _bbox1, const box3& _bbox2 )
 	{
